b2bbad119019276153d5757294eea7c0e4d2ee8d
TheMythologist <leekaixuan2001@gmail.com>
2025-10-29 16:25:30 +0800

    Implement tooltips for all texts

--- src/main/java/greynekos/greybook/ui/PersonTablePanel.java
+++ src/main/java/greynekos/greybook/ui/PersonTablePanel.java
@@ -1,7 +1,6 @@
 package greynekos.greybook.ui;
 
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 import greynekos.greybook.commons.core.LogsCenter;
 import greynekos.greybook.model.person.AttendanceStatus;
@@ -9,10 +8,15 @@ import greynekos.greybook.model.person.Person;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
+import javafx.util.Duration;
 
 /**
  * Panel containing the list of persons.
@@ -45,6 +49,29 @@ public class PersonTablePanel extends UiPart<Region> {
     @FXML
     private TableColumn<Person, String> attendanceStatusColumn;
 
+    public class PersonTableCellWithTooltip extends TableCell<Person, String> {
+        private final Tooltip tooltip = new Tooltip();
+
+        public PersonTableCellWithTooltip() {
+            super();
+            tooltip.setShowDelay(Duration.millis(500));
+            setTooltip(tooltip);
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null || item.isEmpty()) {
+                setText(null);
+                tooltip.setText(null);
+            } else {
+                setText(item);
+                tooltip.setText(item);
+            }
+        }
+    }
+
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
@@ -63,12 +90,19 @@ public class PersonTablePanel extends UiPart<Region> {
         });
 
         nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName().toString()));
+        nameColumn.setCellFactory(col -> new PersonTableCellWithTooltip());
+
         studentIdColumn.setCellValueFactory(
                 cellData -> new SimpleStringProperty(cellData.getValue().getStudentID().toString()));
+        studentIdColumn.setCellFactory(col -> new PersonTableCellWithTooltip());
+
         emailColumn
                 .setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getEmail().toString()));
+        emailColumn.setCellFactory(col -> new PersonTableCellWithTooltip());
+
         phoneColumn
                 .setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPhone().toString()));
+        phoneColumn.setCellFactory(col -> new PersonTableCellWithTooltip());
 
         /*
          * This cellValueFactory is required, as the string is passed to the item
@@ -80,13 +114,16 @@ public class PersonTablePanel extends UiPart<Region> {
 
         // Custom cell factory for tags to display them as styled chips
         tagsColumn.setCellFactory(col -> new TableCell<Person, String>() {
-            private final javafx.scene.layout.FlowPane flowPane = new javafx.scene.layout.FlowPane();
+            private final FlowPane flowPane = new FlowPane();
 
             {
                 flowPane.setId("tags");
                 flowPane.setHgap(4);
                 flowPane.setVgap(4);
-                flowPane.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
+                flowPane.setAlignment(Pos.CENTER_LEFT);
+                setGraphic(flowPane);
+
+                widthProperty().addListener((obs, oldW, newW) -> flowPane.setPrefWrapLength(newW.doubleValue()));
             }
 
             @Override
@@ -94,13 +131,33 @@ public class PersonTablePanel extends UiPart<Region> {
                 super.updateItem(item, empty);
 
                 if (empty || item == null || item.isEmpty()) {
+                    flowPane.getChildren().clear();
                     setGraphic(null);
                 } else {
-                    flowPane.getChildren().clear();
-                    Stream.of(item.split(",")).forEach(tagName -> {
-                        javafx.scene.control.Label tagLabel = new javafx.scene.control.Label(tagName);
-                        flowPane.getChildren().add(tagLabel);
-                    });
+                    // Reuse existing labels if possible
+                    String[] tags = item.split(",");
+                    int existingCount = flowPane.getChildren().size();
+
+                    // Adjust number of labels to match tag count
+                    if (existingCount > tags.length) {
+                        flowPane.getChildren().remove(tags.length, existingCount);
+                    } else {
+                        for (int i = existingCount; i < tags.length; i++) {
+                            Label label = new Label();
+                            Tooltip tooltip = new Tooltip();
+                            tooltip.setShowDelay(Duration.millis(500));
+                            label.setTooltip(tooltip);
+                            flowPane.getChildren().add(label);
+                        }
+                    }
+
+                    // Update label texts & tooltips
+                    for (int i = 0; i < tags.length; i++) {
+                        String tagName = tags[i].trim();
+                        Label label = (Label) flowPane.getChildren().get(i);
+                        label.setText(tagName);
+                        label.getTooltip().setText(tagName);
+                    }
                     flowPane.setPrefWrapLength(getWidth()); // Wrap based on cell width
                     setGraphic(flowPane);
                 }
@@ -112,6 +169,7 @@ public class PersonTablePanel extends UiPart<Region> {
             String display = (status.value == AttendanceStatus.Status.NONE) ? "" : status.toString();
             return new SimpleStringProperty(display);
         });
+        attendanceStatusColumn.setCellFactory(col -> new PersonTableCellWithTooltip());
 
         personTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         personTableView.setItems(personList);
